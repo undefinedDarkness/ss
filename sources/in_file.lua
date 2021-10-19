4613c6bc369b8fcbd8ddfebb86cbce626f44485b
undefinedDarkness <davidnoronha@outlook.in>
2021-10-19 13:11:18 +0530

    icons for in file source

--- sources/in_file.lua
+++ sources/in_file.lua
@@ -1,5 +1,6 @@
 local str= require('util.str')
 local home = os.getenv("HOME")
+local Gio = require('lgi').Gio
 return function (search, force)
 	if (not force) or (#search == 0) then -- Ignore a search for every file!
 		return
@@ -7,8 +8,9 @@ return function (search, force)
 
 	-- Collect output lines
 	-- CHANGE to use a different program to search
+	-- The depth is set to make the search decently fast.
+	-- TODO: Maybe switch to grep or rg for a default...
 	local cmd = io.popen("ag -H -F --depth 2 "..search..' '..home) 
-	-- The depth is set to make the search decently fast...
 	local lines = {}
 	for line in cmd:lines() do
 		table.insert(lines, line)
@@ -22,20 +24,38 @@ return function (search, force)
 		index = index + 1
 		-- Create new widget and add file name...
 		if not current then
-			-- Note: Using an image here, breaks everything for some reason 	
-			current = Gtk.VBox {
-				require('ui.util').class(Gtk.Label { 
-					label = lines[index],
+			-- Note: Using an image here, breaks everything for some reason
+			local icon = Gtk.Image.new_from_gicon(Gio.content_type_get_icon(Gio.content_type_guess(lines[index])), Gtk.IconSize.DND)
+			icon:set_pixel_size(32)
+			
+			local title = Gtk.HBox {homogeneous= false, spacing = 8}
+			title:pack_start(icon, false, false, 0)
+			title:pack_start(require('ui.util').class(Gtk.Label { 
+					label = lines[index]:gsub(home, "~"),
 					xalign = 0,
-				}, 'file-title')
+				}, 'file-title'), false, false, 0)
+			
+			current = Gtk.VBox {
+				title	
 			}
-		-- END
+		
+		-- End current item
 		elseif lines[index] == "" then
-			table.insert(out, { widget = current, name = str.uuid() })
+			table.insert(out, { 
+				widget = current,
+				name = str.uuid(),
+				cb = function()
+					Gio.AppInfo.launch_default_for_uri("file://"..line)
+				end
+			})
 			current = nil
 		else
-			-- Add line and line number
-			current:add(Gtk.Label { label = '		'..str.trim(lines[index]), xalign = 0 })
+			-- Add line with slight margin 
+			local num, line = lines[index]:match("(%d+):(.*)")
+			current:add(Gtk.Label { 
+				label = num..':		'..str.trim(line),
+				xalign = 0 
+			})
 		end
 	end
 
